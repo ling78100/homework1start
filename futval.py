a9d09b3c683046503672dd52b811613fd248f61c
Michael Mandel <mim@mr-pc.org>
2017-08-29 18:22:44 -0400

    added example code

---
+++ futval.py
@@ -0,0 +1,19 @@
+# futval.py
+#    A program to compute the value of an investment
+#    carried 10 years into the future
+#
+# From: http://mcsp.wartburg.edu/zelle/python/ppics3/code/chapter02/futval.py
+
+def main():
+    print("This program calculates the future value")
+    print("of a 10-year investment.")
+
+    principal = eval(input("Enter the initial principal: "))
+    apr = eval(input("Enter the annual interest rate: "))
+
+    for i in range(10):
+        principal = principal * (1 + apr)
+
+    print("The value in 10 years is:", principal)
+
+main()
---
+++ invest.py
@@ -0,0 +1,38 @@
+## Student Name: (YOUR NAME)
+## Date: (TODAY'S DATE)
+## Homework 1, Problem 1, Methods in Computational Linguistics I, Fall 2015
+## 
+## What Does this Program Do:
+## This is a program that computes the future value of a specified amount after n years.
+## The program prints a table to the screen that displays the result.
+## 
+## Inputs:
+## The inputs to this program are ... (YOU SHOULD FINISH THIS COMMENT)
+## 
+## You should know that I discussed this with:
+## IF YOU DISCUSSED AN ASSIGNMENT WITH ANYONE BEFORE SUBMITTING IT, PLEASE
+## LET ME KNOW BY ADDING THAT INFORMATION HERE.  YOU SHOULD NOT WRITE YOUR
+## CODE TOGETHER (YOU SHOULD DO THIS INDEPENDENTLY/SEPARATELY).  HOWEVER,
+## IT IS OK TO DISCUSS PROBLEMS, IN GENERAL, BEFOREHAND. 
+
+def main():
+    
+    # WRITE COMMENTS HERE TO EXPLAIN INTUITIVELY WHAT YOUR PROGRAM
+    # IS DOING STEP-BY-STEP.  FEEL FREE TO ERASE AND CHANGE ANY
+    # OF THE COMMENTS IN HERE.   ALSO, PLEASE DON'T USE ALL 
+    # CAPITAL LETTERS WHEN YOU WRITE YOUR COMMENTS!!!
+        
+    # PERHAPS YOU SHOULD SET UP SOME VARIABLES?
+    
+    # PERHAPS YOU SHOULD GET SOME INPUT FROM THE USER?
+    
+    # PERHAPS YOU SHOULD PRINT A TABLE OF RESULTS?
+
+
+# IN ORDER FOR YOUR CODE TO RUN WHEN SOMEONE TYPES PYTHON INVEST.PY, YOU SHOULD
+# END THE CODE WITH THE WORDS main() WHICH APPEAR BELOW
+main()
+
+
+    
+    
